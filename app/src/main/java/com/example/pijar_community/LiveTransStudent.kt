313cdcb401d5494821a9ee55a8629e2302198d0f
gfrdausy <gfirdausy@gmail.com>
2023-11-06 21:51:03 +0700

    Community 50% LiveTrans -> student Room on progress

---
+++ app/src/main/java/com/example/pijar_community/LiveTransStudent.kt
@@ -0,0 +1,11 @@
+package com.example.pijar_community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LiveTransStudent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_live_trans_student)
+    }
+}
